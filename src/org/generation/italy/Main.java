dc7a87cee585a008867d18a893799d9ce93d7220
USER <USER@DESKTOP-IC4S2OB.station>
2024-05-30 13:20:52 +0200

    Quarta versione
    Aggiunta la possibilità di modificare un libro esistente nel DB.

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -15,7 +15,15 @@ import org.generation.italy.model.Libri;
 class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* TODO :
+		 * Sistemare la ricerca dell'id!!!
+		 * Sistemare PROGRAMMA 2:
+		 *  - Aggiungere filtri di ricerca;
+		 *  - Aggiungere visualizzazione autori, editori e generi.
+		 * Aggiungere altre possibilità di ricerca nel PROGRAMMA 4.
+		 * Creare una funzione ANNULLA da usare in qualsiasi momento
+		 * Creare una funzione di aggiornamento dati locali (lista elencoLibri)
+		 */
 		Scanner sc= new Scanner(System.in);
 		String scelta = new String();
 		Libri libro;
@@ -65,7 +73,7 @@ class Main {
 				case "1":
 					//lo lascio andare al caso successivo
 				case "inserimento nuovo libro":
-					System.out.println("Inserimento nuovo libro nel DataBase.\n");
+					System.out.println("\n\n\nInserimento nuovo libro nel DataBase.\n");
 					
 					//chiedo il titolo
 					System.out.print("Inserire il titolo del libro: ");
@@ -162,6 +170,7 @@ class Main {
 				case "2":
 					//passo al caso successivo
 				case "visualizza libri esistenti":
+					System.out.println("\n\n\nVisualizzazione libri");
 					//scorro la lista dei libri per mostrarli
 					int contatore=0;
 					//uso il ciclo for-each per mostrare ogni elemento della lista elencoLibri
@@ -180,7 +189,7 @@ class Main {
 				case "3":
 					//passo direttamente al caso successivo
 				case "cancellazione libro":
-					System.out.println("Cancellazione libro");
+					System.out.println("\n\n\nCancellazione libro");
 					System.out.println("Inserire il titolo del libro da rimuovere:");
 					String titoloRimozione=sc.nextLine();
 					System.out.println("Connessione al DataBase in corso...");
@@ -214,6 +223,143 @@ class Main {
 					}catch (Exception e) {
 						System.err.println("Error: "+e);
 					}
+					break;
+				//		----- CASO 4 -----
+				case "4":
+					//passo al caso successivo
+				case "modifica libro esistente":
+					System.out.println("\n\n\nModifica di un libro presente nel DataBase");
+					
+					int idModifica=-1;
+					int contaLibri;
+					//creo la lista dei libri cercati da usare dopo
+					ArrayList <Libri> libriCercati=new ArrayList<Libri>();
+					System.out.println("Connessione al DataBase in corso...");
+					try (Connection conn=DriverManager.getConnection(url, "root", "")) {
+						System.out.println("Connessione riuscita!");
+						System.out.print("\nInserire il titolo del libro di cui modificare i dati: ");
+						String titoloModifica=sc.nextLine();
+						System.out.println("\nRicerca del libro in corso...");
+						String sql="SELECT * FROM libri WHERE titolo LIKE ?";
+						try (PreparedStatement ricerca=conn.prepareStatement(sql)) {
+							//faccio la ricerca dei libri che potrebbero corrispondere con quel titolo
+							ricerca.setString(1, "%"+titoloModifica+"%");
+							try (ResultSet cercati=ricerca.executeQuery()){
+								contaLibri=0;
+								l=new Libri();
+								while (cercati.next()) {
+									//conto quanti risultati ho trovato per i vari casi e li aggiungo alla lista
+									l=new Libri();
+									contaLibri++;
+									l.id=cercati.getInt("id_libri");
+									l.titolo=cercati.getString("titolo");
+									libriCercati.add(l);
+								}
+								if (contaLibri==0) {
+									//il titolo inserito non corrisponde a nessuno dei presenti nel DB
+									System.out.println("Libro non trovato");
+								}else {
+									//caso in cui ho trovato almeno una corrispondenza
+									if (contaLibri==1) {
+										//ho trovato solo una corrispondenza tra titolo inserito e libri nel DB
+										System.out.println("Il libro trovato è "+l.titolo+" e il suo ID è: "+l.id);
+										idModifica=l.id;
+									}else if (contaLibri>1) {
+										//ho trovato più corrispondenze nel DB => chiedo quale sia quella giusta
+										System.out.println("Sono stati trovati più libri:");
+										for (int k=0; k<libriCercati.size(); k++) {
+											//mostro i titoli trovati
+											System.out.println((k+1)+") "+libriCercati.get(k).titolo);
+										}
+										do {
+											//chiedo quale sia il titolo corretto e controllo il corretto inserimento
+											idModifica=-1;
+											System.out.println("\nScrivi il titolo completo del libro che vuoi cambiare");
+											titoloModifica=sc.nextLine();
+											for(int i=0; i<libriCercati.size(); i++) {
+												if (libriCercati.get(i).titolo.equals(titoloModifica)) {
+													System.out.println("\nLibro selezionato: "+libriCercati.get(i).titolo);
+													idModifica=libriCercati.get(i).id;
+												}
+												
+											}
+											if (idModifica==-1)
+												System.out.println("ATTENZIONE! Inserimento sbagliato, riprova!");
+										} while (idModifica==-1);
+									}
+									//ID del titolo da modificare trovato => procedo con l'UPDATE
+									
+									//ora chiedo di inserire i nuovi dati del libro
+									System.out.println("Inserire i nuovi dati del libro");
+									Libri modifica=new Libri();
+									
+									modifica.id=idModifica;
+									//cerco la posizione del libro nella lista locale
+									int pos=0;
+									for (int j=0; j<elencoLibri.size(); j++) {
+										if (elencoLibri.get(j).id==idModifica) 
+											pos=j;
+									}
+									
+									//chiedo il titolo
+									System.out.print("Inserire il titolo: ");
+									modifica.titolo=sc.nextLine();
+									//chiedo l'autore
+									System.out.print("Inserire l'autore: ");
+									String autore=sc.nextLine();
+									modifica.idAutore=trovaId(conn, "SELECT id_autori FROM autori WHERE CONCAT(nome,' ',cognome) = ?;", autore, "id_autori");
+									System.out.println("Id autore: "+modifica.idAutore);
+									//chiedo il genere
+									System.out.print("Inserire il genere del libro: ");
+									String genere=sc.nextLine();
+									modifica.idGenere=trovaId(conn, "SELECT id_generi FROM generi WHERE nome = ?;", genere, "id_generi");
+									System.out.println("Id genere: "+modifica.idGenere);
+									//chiedo la data di pubblicazione
+									System.out.print("Inserire la data di pubblicazione: ");
+									modifica.pubblicazione = LocalDate.parse(sc.nextLine(), df);
+									//chiedo il numero di pagine
+									System.out.print("Inserire il numero di pagine: ");
+									modifica.numPagine=sc.nextInt();
+									sc.nextLine();
+									//chiedo la quantità
+									System.out.print("Inserire la quantità aggiunta: ");
+									modifica.qnt=sc.nextInt();
+									sc.nextLine();
+									//chiedo l'editore
+									System.out.print("Inserire il nome dell'editore: ");
+									String editore=sc.nextLine();
+									modifica.idEditore=trovaId(conn, "SELECT id_editori FROM editori WHERE nome = ?;", editore, "id_editori");
+									System.out.println("Id editore: "+modifica.idEditore);
+									
+									sql="UPDATE libri SET titolo = ?, id_autore = ?, id_genere = ?, data_pubblicazione = ?, num_pagine = ?, qnt = ?, id_editore = ? WHERE id_libri = ?";
+									try (PreparedStatement update=conn.prepareStatement(sql)) {
+										//preparo l'istruzione sql per aggiornare i dati
+										update.setString(1, modifica.titolo);
+										update.setInt(2, modifica.idAutore);
+										update.setInt(3, modifica.idGenere);
+										update.setDate(4, Date.valueOf(modifica.pubblicazione));
+										update.setInt(5, modifica.numPagine);
+										update.setInt(6, modifica.qnt);
+										update.setInt(7, modifica.idEditore);
+										update.setInt(8, modifica.id);
+										
+										//invio l'ordine di eseguire l'aggiornamento
+										int righeInteressate=update.executeUpdate();
+										elencoLibri.set(pos, modifica);
+										if (righeInteressate>0) {
+											System.out.println("Aggiornamento avvenuto con successo!");
+										}
+									}
+								}
+							}
+						}
+						
+					}catch (Exception e) {
+						System.err.println("Error: "+e);
+					}
+					
+					break;
+				//		----- CASO 5 -----
 				case "5":
 					//passo al successivo
 				case "esci":
@@ -229,10 +375,8 @@ class Main {
 		int id=0;
 		try (PreparedStatement ps=conn.prepareStatement(sql)) {
 			ps.setString(1, cercare);
-			System.out.println(ps.toString());
 			try (ResultSet rs=ps.executeQuery()) {
 				while (rs.next()) {
-					System.out.println(rs.getInt(argomento));
 					id=rs.getInt(argomento);
 				}
 			}
